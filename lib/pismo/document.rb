f9c54ad120c3a33326a65ca12a584b4644fde94e
Arkadiusz Buras <macbury@gmail.com>
2012-09-15 17:09:07 +0300

    Update lib/pismo/document.rb

--- lib/pismo/document.rb
+++ lib/pismo/document.rb
@@ -47,7 +47,8 @@ module Pismo
     def self.clean_html(html)
       # Normalize stupid entities
       # TODO: Optimize this so we don't need all these sequential gsubs
-      html = html.encode("UTF-8")
+      html.encode!("UTF-8")
+      html = html.unpack('C*').pack('U*')
       html.gsub!("&#8194;", " ")
       html.gsub!("&#8195;", " ")
       html.gsub!("&#8201;", " ")
